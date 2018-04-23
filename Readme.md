1bd1418b7c86175778d5e2ddd9d484d17b33f55a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Activate the Instant Feedback Mode for the Grid Bound to an Entity Framework model
+
+
+<p>This example shows how to implement the Instant Feedback Mode (asynchronous data loading) for a grid bound to Entity Framework 4.0 Classes. To activate this feature, use our specially designed data source (EntityInstantFeedbackDataSource).<br /><br />To start this example connect the Entity Data Model to the Northwind database.</p>
+
+<br/>
+
+
---
+++ VB/dxSample-InstanceFeedbackMode/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Data.Linq
+Imports DevExpress.Xpf.Grid
+
+Namespace dxSample_InstanceFeedbackMode
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Private dataContext As NorthwindEntities
+		Public Sub New()
+			InitializeComponent()
+			dataContext = New NorthwindEntities()
+			entitySource.QueryableSource = dataContext.Products
+		End Sub
+	End Class
+End Namespace
